854ac8b768949db163ca222c4092daf36753508c
Ghazanfar Shah Uzair <dev.uzair@gmail.com>
2026-06-19 23:45:06 +0200

    float64 values

---
+++ investment_calculator.go
@@ -0,0 +1,12 @@
+package main
+
+import "math"
+
+func main() {
+	var investmentAmout = 1000
+	var expectedReturnRate = 5.5
+	var years = 10
+
+	var futureValue = float64(investmentAmout) * math.Pow(1+expectedReturnRate/100, float64(years))
+
+}
