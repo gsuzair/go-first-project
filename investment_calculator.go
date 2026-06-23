8d29bd067c62324c91d598527609ff297f547d0f
Ghazanfar Shah Uzair <dev.uzair@gmail.com>
2026-06-23 05:05:36 +0200

    user input fetch, fmt.scan

--- investment_calculator.go
+++ investment_calculator.go
@@ -1,12 +1,29 @@
 package main
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	var investmentAmout = 1000
-	var expectedReturnRate = 5.5
-	var years = 10
+	const inflationRate = 2.5
+	var investmentAmout float64 = 1000
+	expectedReturnRate := 5.5
+	var years float64
 
-	var futureValue = float64(investmentAmout) * math.Pow(1+expectedReturnRate/100, float64(years))
+	fmt.Print("Investment Amount: ")
+	fmt.Scan(&investmentAmout)
+
+	fmt.Print("Expected Return Rate (in %): ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
+
+	futureValue := float64(investmentAmout) * math.Pow(1+expectedReturnRate/100, float64(years))
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+
+	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 
 }
